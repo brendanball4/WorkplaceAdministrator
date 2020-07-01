1fa22049eb5fc8e6c9894eba1d092de7f4327929
Brendanball4 <ball.brendan10@gmail.com>
2020-06-30 22:05:44 -0600

    BlazorApp Initializing

---
+++ WorkplaceAdministrator.Blazor/Client/SchedulingServices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkplaceAdministrator.Blazor.Repositories;
+using WorkplaceAdministrator.Common;
+using WorkplaceAdministrator.Common.Dtos;
+
+namespace WorkplaceAdministrator.Blazor
+{
+    public interface ISchedulingService
+    { 
+        Task<List<ShiftReadEditDto>> GetAllShifts();
+        Task<List<AccountAdminListDto>> GetAllAccounts();
+        Task<List<Position>> GetAllPositions();
+    }
+
+    public class SchedulingService : ISchedulingService
+    {
+        private readonly IShiftRepo _shiftRepo;
+        private readonly IAccountRepo _accountRepo;
+        private readonly IPositionRepo _positionRepo;
+
+        public Task<List<AccountAdminListDto>> GetAllAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Position>> GetAllPositions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ShiftReadEditDto>> GetAllShifts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WorkplaceAdministrator.Common.Clients/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkplaceAdministrator.Common.Clients
+{
+    public class UserInfo
+    {
+        public string Id { get; set; }
+        public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
